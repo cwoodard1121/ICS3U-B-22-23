e35cf043b57710a218b4e9539854ba4fecfdcd6e
Cameron Woodard <cwoodard@bayviewglen.ca>
2022-09-09 11:01:57 -0400

    added day2 exampleone and exampletwo classes

---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleOne.java
@@ -0,0 +1,23 @@
+package day2;
+
+public class ExampleOne {
+    
+    public static void main(String[] args) {
+        System.out.println("Welcome to computer science");
+        System.out.println("welcome to computer science");
+        System.out.println("welcome to computer science");
+        
+        System.out.println();
+
+        System.out.print("A");
+        System.out.print("B");
+        System.out.print("C");
+
+        System.out.println("D");
+        System.out.println("E");
+        System.out.println("F");
+    }
+
+
+}
+
---
+++ Unit One - Introduction to Computer Science/src/day2/ExampleTwo.java
@@ -0,0 +1,7 @@
+package day2;
+
+public class ExampleTwo {
+    public static void main(String[] args) {
+        System.out.println("Computer Science is the \"BEST\" Class ever!");
+    }
+}
